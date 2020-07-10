c485f47229774a04bf616d8c11204d76cc43dafb
alam1386 <alam1386@gmail.com>
2020-07-10 16:32:59 -0400

    Updates

---
+++ precache-manifest.66b0e3b239b4a9275f578d2678b9aea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd0d974cc3678dfb0fcce09c5b2c3c3",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "a9d73dc63ad57e49b392",
+    "url": "/expense-tracker/static/css/main.22368a26.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d73dc63ad57e49b392",
+    "url": "/expense-tracker/static/js/main.909db2a3.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
